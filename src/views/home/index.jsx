048de438200995d1225f745836ed9b2d40b7c172
huyKirito <huyyhuqqho@gmail.com>
2024-04-27 14:40:32 +0700

    add event hubs dep, eventhubs function and env

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,64 +1,73 @@
-import { ArrowRightOutlined } from '@ant-design/icons';
-import { MessageDisplay } from '@/components/common';
-import { ProductShowcaseGrid } from '@/components/product';
-import { FEATURED_PRODUCTS, RECOMMENDED_PRODUCTS, SHOP } from '@/constants/routes';
+import { ArrowRightOutlined } from '@ant-design/icons'
+import { MessageDisplay } from '@/components/common'
+import { ProductShowcaseGrid } from '@/components/product'
 import {
-  useDocumentTitle, useFeaturedProducts, useRecommendedProducts, useScrollTop
-} from '@/hooks';
-import bannerImg from '@/images/banner-girl.png';
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+  FEATURED_PRODUCTS,
+  RECOMMENDED_PRODUCTS,
+  SHOP
+} from '@/constants/routes'
+import {
+  useDocumentTitle,
+  useFeaturedProducts,
+  useRecommendedProducts,
+  useScrollTop
+} from '@/hooks'
+import bannerImg from '@/images/banner-girl.png'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  useDocumentTitle('Salinaka | Home');
-  useScrollTop();
+  useDocumentTitle('Salinaka | Home')
+  useScrollTop()
 
   const {
     featuredProducts,
     fetchFeaturedProducts,
     isLoading: isLoadingFeatured,
     error: errorFeatured
-  } = useFeaturedProducts(6);
+  } = useFeaturedProducts(6)
   const {
     recommendedProducts,
     fetchRecommendedProducts,
     isLoading: isLoadingRecommended,
     error: errorRecommended
-  } = useRecommendedProducts(6);
+  } = useRecommendedProducts(6)
 
   return (
-    <main className="content">
-      <div className="home">
-        <div className="banner">
-          <div className="banner-desc">
-            <h1 className="text-thin">
+    <main className='content'>
+      <div className='home'>
+        <div className='banner'>
+          <div className='banner-desc'>
+            <h1 className='text-thin'>
               <strong>See</strong>
               &nbsp;everything with&nbsp;
               <strong>Clarity</strong>
             </h1>
             <p>
-              Buying eyewear should leave you happy and good-looking, with money in your pocket.
-              Glasses, sunglasses, and contacts—we’ve got your eyes covered.
+              Buying eyewear should leave you happy and good-looking, with money
+              in your pocket. Glasses, sunglasses, and contacts—we’ve got your
+              eyes covered.
             </p>
             <br />
-            <Link to={SHOP} className="button">
+            <Link to={SHOP} className='button'>
               Shop Now &nbsp;
               <ArrowRightOutlined />
             </Link>
           </div>
-          <div className="banner-img"><img src={bannerImg} alt="" /></div>
+          <div className='banner-img'>
+            <img src={bannerImg} alt='' />
+          </div>
         </div>
-        <div className="display">
-          <div className="display-header">
+        <div className='display'>
+          <div className='display-header'>
             <h1>Featured Products</h1>
             <Link to={FEATURED_PRODUCTS}>See All</Link>
           </div>
-          {(errorFeatured && !isLoadingFeatured) ? (
+          {errorFeatured && !isLoadingFeatured ? (
             <MessageDisplay
               message={errorFeatured}
               action={fetchFeaturedProducts}
-              buttonLabel="Try Again"
+              buttonLabel='Try Again'
             />
           ) : (
             <ProductShowcaseGrid
@@ -67,16 +76,16 @@ const Home = () => {
             />
           )}
         </div>
-        <div className="display">
-          <div className="display-header">
+        <div className='display'>
+          <div className='display-header'>
             <h1>Recommended Products</h1>
             <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
           </div>
-          {(errorRecommended && !isLoadingRecommended) ? (
+          {errorRecommended && !isLoadingRecommended ? (
             <MessageDisplay
               message={errorRecommended}
               action={fetchRecommendedProducts}
-              buttonLabel="Try Again"
+              buttonLabel='Try Again'
             />
           ) : (
             <ProductShowcaseGrid
@@ -87,7 +96,7 @@ const Home = () => {
         </div>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
